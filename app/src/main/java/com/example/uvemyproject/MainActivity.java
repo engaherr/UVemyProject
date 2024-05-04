005d1ab595782b5fc069afdec6f878a43a353583
Enrique Gamboa Hernández <113001293+engaherr@users.noreply.github.com>
2024-05-04 11:01:43 -0600

    pr(master): Añadir menú de navegación inferior (#1)
    * feat(build.gradle):Actualizar dependencia "material" a 1.12.0
    
    * feat(layout): Agregar fragmento de formulario de usuario y su diseño XML
    
    * feat(java/res): Agregar BottomNavigationBar

--- app/src/main/java/com/example/uvemyproject/MainActivity.java
+++ app/src/main/java/com/example/uvemyproject/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.uvemyproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.uvemyproject.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int CURSOS_ID = R.id.cursos;
+    final int BUSQUEDA_ID = R.id.busqueda;
+    final int PERFIL_ID = R.id.perfil;
+
+    ActivityMainBinding binding;
+
+    BottomNavigationView bottomNavigationView;
+
+    FormularioUsuario formularioUsuario = new FormularioUsuario();
+    FormularioCurso formularioCurso = new FormularioCurso();
+    CursoDetallesInformacion cursoDetallesInformacion = new CursoDetallesInformacion();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(
+                R.id.container, formularioUsuario).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == CURSOS_ID) {
+                    getSupportFragmentManager().beginTransaction().replace(
+                            R.id.container, formularioCurso).commit();
+                    return true;
+                } else if (itemId == BUSQUEDA_ID) {
+                    getSupportFragmentManager().beginTransaction().replace(
+                            R.id.container, cursoDetallesInformacion).commit();
+                    return true;
+                } else if (itemId == PERFIL_ID) {
+                    getSupportFragmentManager().beginTransaction().replace(
+                            R.id.container, formularioUsuario).commit();
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
