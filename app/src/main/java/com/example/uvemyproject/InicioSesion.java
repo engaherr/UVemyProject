d94294cc9dcd5f6b088760b61e20b893a064a23e
engaherr <kikgamboa@gmail.com>
2024-06-11 17:29:16 -0600

    feat: Inicio de sesión con validaciones asignando SingletonUsuario agregado

--- app/src/main/java/com/example/uvemyproject/InicioSesion.java
+++ app/src/main/java/com/example/uvemyproject/InicioSesion.java
@@ -1,14 +1,155 @@
 package com.example.uvemyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.uvemyproject.api.ApiClient;
+import com.example.uvemyproject.api.services.AutenticacionServices;
+import com.example.uvemyproject.databinding.ActivityInicioSesionBinding;
+import com.example.uvemyproject.dto.CredencialesDTO;
+import com.example.uvemyproject.dto.UsuarioDTO;
+import com.example.uvemyproject.utils.CredencialesValidador;
+import com.example.uvemyproject.utils.SingletonUsuario;
+import com.example.uvemyproject.viewmodels.InicioSesionViewModel;
 
 public class InicioSesion extends AppCompatActivity {
 
+    private ActivityInicioSesionBinding binding;
+    private InicioSesionViewModel viewModel;
+
+    @SuppressLint("ShowToast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio_sesion);
+        binding = ActivityInicioSesionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewModel = new ViewModelProvider(this).get(InicioSesionViewModel.class);
+
+        binding.edtTextCorreoElectronico.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.txtViewMensajeError.setText("");
+            }
+        });
+
+        binding.edtTextContrasena.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.txtViewMensajeError.setText("");
+            }
+        });
+
+        binding.btnIniciarSesion.setOnClickListener(c -> {
+            resetearCampos();
+            if (validarCampos()) {
+                iniciarSesion(String.valueOf(binding.edtTextCorreoElectronico.getText()).trim(),
+                        String.valueOf(binding.edtTextContrasena.getText()).trim());
+            }
+        });
+
+        observarStatus();
+
+        observarUsuario();
+    }
+
+    private void observarUsuario() {
+        viewModel.getUsuarioActual().observe(this, usuario -> {
+            if (usuario != null) {
+                String mensajeExito = "Bienvenido a UVemy " + usuario.getNombres() +
+                        " " + usuario.getApellidos();
+                Toast.makeText(this,mensajeExito, Toast.LENGTH_LONG).show();
+                SingletonUsuario.setIdUsuario(usuario.getIdUsuario());
+                SingletonUsuario.setNombres(usuario.getNombres());
+                SingletonUsuario.setApellidos(usuario.getApellidos());
+                SingletonUsuario.setCorreoElectronico(usuario.getCorreoElectronico());
+                SingletonUsuario.setIdsEtiqueta(usuario.getIdsEtiqueta());
+                SingletonUsuario.setJwt(usuario.getJwt());
+                redireccionarMenuPrincipal();
+            }
+        });
+    }
+
+    private void observarStatus(){
+        viewModel.getStatus().observe(this, status -> {
+            switch (status) {
+                case ERROR:
+                    binding.txtViewMensajeError.setText("Correo electrónico o contraseña incorrectos");
+                    break;
+                case ERROR_CONEXION:
+                    Toast.makeText(this,"Ocurrió un error de conexión con el servidor", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            quitarEspera();
+        });
+    }
+
+    private void redireccionarMenuPrincipal() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void resetearCampos() {
+        binding.edtTextCorreoElectronico.setBackgroundResource(R.drawable.background_lightblue);
+        binding.edtTextContrasena.setBackgroundResource(R.drawable.background_lightblue);
+    }
+
+    private boolean validarCampos() {
+        boolean sonCamposValidos = true;
+        String mensajeError = "";
+        String correoElectronico = String.valueOf(binding.edtTextCorreoElectronico.getText());
+        String contrasena = String.valueOf(binding.edtTextContrasena.getText());
+
+        if (correoElectronico.trim().isEmpty()) {
+            sonCamposValidos = false;
+            mensajeError = "Correo electrónico requerido";
+            binding.edtTextCorreoElectronico.setBackgroundResource(R.drawable.background_errorcampo);
+        } else if (!CredencialesValidador.esEmailValido(correoElectronico)) {
+            sonCamposValidos = false;
+            mensajeError = "Correo electrónico inválido";
+        }
+
+        if (contrasena.trim().isEmpty()) {
+            sonCamposValidos = false;
+            mensajeError += mensajeError.isEmpty() ? "Contraseña requerida" : " y la contraseña es requerida";
+            binding.edtTextContrasena.setBackgroundResource(R.drawable.background_errorcampo);
+        }
+
+        if (!mensajeError.isEmpty()) {
+            Toast.makeText(this,mensajeError,Toast.LENGTH_LONG).show();
+        }
+        return sonCamposValidos;
+    }
+
+    private void iniciarSesion(String correoElectronico, String contrasena) {
+        ponerEspera();
+        viewModel.iniciarSesion(correoElectronico, contrasena);
+    }
+
+    private void ponerEspera() {
+        binding.progressOverlay.setVisibility(View.VISIBLE);
+    }
+
+    private void quitarEspera() {
+        binding.progressOverlay.setVisibility(View.GONE);
     }
 }
