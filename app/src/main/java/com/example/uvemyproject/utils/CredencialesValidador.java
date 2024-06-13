ad5de84a5c791a984f637571c3a19243ffd22fe5
engaherr <kikgamboa@gmail.com>
2024-06-12 18:31:17 -0600

    feat(utils): Añadir para validar políticas de contraseña segura

--- app/src/main/java/com/example/uvemyproject/utils/CredencialesValidador.java
+++ app/src/main/java/com/example/uvemyproject/utils/CredencialesValidador.java
@@ -2,8 +2,36 @@ package com.example.uvemyproject.utils;
 
 import android.text.TextUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class CredencialesValidador {
     public static boolean esEmailValido(CharSequence target) {
         return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public static boolean esContrasenaSegura(String contrasena) {
+        boolean esContrasenaSegura;
+        String patron = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$";
+
+        long tiempoLimite = 500;
+
+        try {
+            Pattern pattern = Pattern.compile(patron);
+            Matcher matcher = pattern.matcher(contrasena);
+
+            long startTime = System.currentTimeMillis();
+            esContrasenaSegura = matcher.find();
+            long endTime = System.currentTimeMillis();
+
+            if ((endTime - startTime) > tiempoLimite) {
+                throw new PatternSyntaxException("Time limit exceeded", patron, -1);
+            }
+        } catch (PatternSyntaxException e) {
+            esContrasenaSegura = false;
+        }
+
+        return esContrasenaSegura;
     }
 }
